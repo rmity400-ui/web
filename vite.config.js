12ea125156bec228492f39cffb3439f53c29f4e7
school-website-app-now <rmity400@gmail.com>
2026-05-25 13:01:24 +0700

    The Update Website new coding

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'; // ត្រូវប្រាកដថាបាន install package នេះ
-
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     tailwindcss(), // ដាក់វាចូលត្រង់នេះ
